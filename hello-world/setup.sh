94213506d84e33846b91b0c5914220b44f9a5891
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-14 20:45:31 +0100

    Attempting to get files into home area

--- hello-world/setup.sh
+++ hello-world/setup.sh
@@ -1,4 +1,4 @@
-mkdir /opt/softwareag;
-curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag;
-. /opt/softwareag/Apama/bin/apama_env
-cd ~
+#mkdir /opt/softwareag;
+#curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag;
+#. /opt/softwareag/Apama/bin/apama_env
+#cd ~
