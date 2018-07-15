71ed25530638663d81adfabf23d8b87414f84e9b
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-15 20:55:14 +0100

    Attempting to auto open main.mon

--- simple-apama/setup.sh
+++ simple-apama/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hostname
-scp -r root@host01:~/apama-project ~/apama-project
+scp -r ~/apama-project root@host01:~/apama-project
 ssh root@host01 "mkdir /opt/softwareag"
 ssh root@host01 "curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag"
 ssh root@host01 "touch /opt/softwareag/apama.installed"
