b26db457f6e1646e1066c3214f20fece4cc07053
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-15 10:11:21 +0100

    getting first project ready

--- simple-apama/setup.sh
+++ simple-apama/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh root@host01 "mkdir /opt/softwareag"
 ssh root@host01 "curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag"
-ssh root@host01 "sudo wall -n 'Apama Installed and Ready!'"
+ssh root@host01 "sudo echo 'Apama installed and ready!\n$ ' > /dev/pts/0"
