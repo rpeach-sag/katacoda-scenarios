d4e00e5c6d46f63ae9f186f2faf695140f95b4bc
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-15 09:18:58 +0100

    Trying to get courseData to run

--- simple-apama/setup.sh
+++ simple-apama/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir /opt/softwareag
-curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag
-touch /opt/softwareag/apama.installed
+ssh root@host01 "mkdir /opt/softwareag"
+ssh root@host01 "curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag"
+ssh root@host01 "touch /opt/softwareag/apama.installed"
