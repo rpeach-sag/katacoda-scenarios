be458f8f533b5c4a2004d336a6bcb4bc8928e3f1
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-15 10:05:24 +0100

    getting first project ready

--- simple-apama/set-env.sh
+++ simple-apama/set-env.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 cd /root/apama-project
-until [ -f /opt/softwareag/apama.installed ]; do sleep 1; done
--- simple-apama/setup.sh
+++ simple-apama/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh root@host01 "mkdir /opt/softwareag"
 ssh root@host01 "curl https://downloads.apamacommunity.com/apama-core/10.2.0.2/apama_core_10.2.0.2_amd64_linux.tar.gz | tar -zx -C /opt/softwareag"
-ssh root@host01 "touch /opt/softwareag/apama.installed"
+ssh root@host01 "sudo wall 'Apama Installed and Ready!'"
