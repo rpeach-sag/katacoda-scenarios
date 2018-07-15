2778bf811e1b735ecb49ba65786065ff579727e2
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-07-15 11:03:51 +0100

    Showing info text while installing

--- simple-apama/set-env.sh
+++ simple-apama/set-env.sh
@@ -1,3 +1,3 @@
 JAVA_HOME=/jre
 cd /root/apama-project
-clear && printf '\u001b[32mApama is installing...\u001b[m\r\n'
+clear && printf '\u001b[32mApama is installing...\u001b[m' && { until [ -f /opt/softwareag/apama.installed ]; do sleep 1; done } && printf 'Done\r\n'
